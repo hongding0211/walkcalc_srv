45dc0350c0e06c621da8dc01609a03b67aad95d1
HongDing <keith.dh@hotmail.com>
2023-03-01 19:02:46 +0800

    feat: group controller

---
+++ app/controller/group.js
@@ -0,0 +1,89 @@
+const BaseController = require('./base')
+
+class GroupController extends BaseController {
+  async create() {
+    this.ctx.validate(
+      {
+        members: { type: 'array', required: false },
+      },
+      this.ctx.request.body
+    )
+
+    const members = this.ctx.request.body.members || []
+
+    const c = await this.ctx.service.group.create(members)
+    this.success({
+      groupId: c[0].id,
+    })
+  }
+
+  async join() {
+    this.ctx.validate(
+      {
+        id: { type: 'string' },
+      },
+      this.ctx.request.body
+    )
+
+    const { id } = this.ctx.request.body
+
+    try {
+      const update = await this.ctx.service.group.join(id)
+      if (update.nModified === 1) {
+        this.success({
+          groupId: id,
+        })
+      } else {
+        this.error('Join failed.')
+      }
+    } catch (e) {
+      this.error(e.message)
+    }
+  }
+
+  async dismiss() {
+    this.ctx.validate(
+      {
+        id: { type: 'string' },
+      },
+      this.ctx.query
+    )
+
+    const { id } = this.ctx.query
+
+    const del = await this.ctx.service.group.dismiss(id)
+    if (del.deletedCount > 0) {
+      this.success({
+        groupId: id,
+      })
+    } else {
+      this.error('Dismiss failed.')
+    }
+  }
+
+  async clear() {
+    this.ctx.validate(
+      {
+        id: { type: 'string' },
+      },
+      this.ctx.request.body
+    )
+
+    const { id } = this.ctx.request.body
+
+    try {
+      const update = await this.ctx.service.group.clear(id)
+      if (update.n === 1) {
+        this.success({
+          groupId: id,
+        })
+      } else {
+        this.error('Clear failed.')
+      }
+    } catch (e) {
+      this.error(e.message)
+    }
+  }
+}
+
+module.exports = GroupController
---
+++ app/service/group.js
@@ -0,0 +1,136 @@
+const Service = require('egg').Service
+
+class GroupService extends Service {
+  async create(members) {
+    const { _id } = this.ctx.token
+    const userId = new this.app.mongoose.Types.ObjectId(_id)
+
+    const c = await this.ctx.model.Group.find({}, { id: 1 })
+      .sort({ _id: -1 })
+      .limit(-1)
+
+    let nextIdNumber
+    if (c.length < 1) {
+      nextIdNumber = 40960 // 0xA000
+    } else {
+      nextIdNumber = parseInt(c[0].id, 16) + 1
+    }
+
+    const nextId = nextIdNumber.toString(16).toUpperCase()
+
+    let membersIds
+    if (!members || !members.length) {
+      membersIds = []
+    } else {
+      const f = await this.ctx.model.User.find(
+        {
+          uuid: { $in: members },
+          _id: { $ne: userId },
+        },
+        {
+          _id: 1,
+        }
+      )
+      membersIds = f.map((e) => e._id)
+    }
+
+    return await this.ctx.model.Group.insertMany([
+      {
+        id: nextId,
+        owner: _id,
+        members: membersIds,
+        record: [],
+        createdAt: Date.now(),
+        modifiedAt: Date.now(),
+      },
+    ])
+  }
+
+  async join(groupId) {
+    const { _id } = this.ctx.token
+    const userId = new this.app.mongoose.Types.ObjectId(_id)
+
+    if (
+      (
+        await this.ctx.model.Group.find({
+          id: groupId,
+        })
+      ).length < 1
+    ) {
+      throw new Error('Group not exists.')
+    }
+
+    if (
+      (
+        await this.ctx.model.Group.find({
+          id: groupId,
+          members: {
+            $elemMatch: { $eq: userId },
+          },
+        })
+      ).length > 0
+    ) {
+      throw new Error('User already in this group.')
+    }
+
+    if (
+      (
+        await this.ctx.model.Group.find({
+          id: groupId,
+          owner: userId,
+        })
+      ).length > 0
+    ) {
+      throw new Error('You are already the group owner.')
+    }
+
+    return this.ctx.model.Group.updateOne(
+      {
+        id: groupId,
+      },
+      {
+        $push: {
+          members: userId,
+        },
+      }
+    )
+  }
+
+  async dismiss(groupId) {
+    const { _id } = this.ctx.token
+    const userId = new this.app.mongoose.Types.ObjectId(_id)
+
+    return this.ctx.model.Group.deleteOne({
+      id: groupId,
+      owner: userId,
+    })
+  }
+
+  async clear(groupId) {
+    const { _id } = this.ctx.token
+    const userId = new this.app.mongoose.Types.ObjectId(_id)
+
+    if (
+      (
+        await this.ctx.model.Group.find({
+          id: groupId,
+          owner: userId,
+        })
+      ).length < 1
+    ) {
+      throw new Error('You do not own this group')
+    }
+
+    return this.ctx.model.Group.updateOne(
+      {
+        id: groupId,
+        owner: userId,
+      },
+      {
+        $set: { record: [] },
+      }
+    )
+  }
+}
+
+module.exports = GroupService
