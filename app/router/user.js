13e06bb93c4b3c3a1ccfe63caaa1a48d6af314d6
Hong <keith.dh@hotmail.com>
2023-06-26 22:55:48 +0800

    feat: patch user info (#8)

--- app/router/user.js
+++ app/router/user.js
@@ -4,6 +4,7 @@
 module.exports = ({ router, controller }) => {
   router.get('/user/login', controller.user.login)
   router.get('/user/info', controller.user.info)
+  router.patch('/user/info', controller.user.patchInfo)
   router.post('/user/infos', controller.user.infos)
   router.get('/user/search', controller.user.search)
   router.get('/user/myDebt', controller.user.myDebt)
